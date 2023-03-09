1aa465868f60bf95cf77c5ae15b62ff49b545a1c
Alexis Lara <alexislaracamacho@my.whccd.edu>
2023-03-08 19:14:37 -0800

    Add project files.

---
+++ COMP-003AFinalProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP_003AFinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
